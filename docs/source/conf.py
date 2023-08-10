63979bdc278a2acbc58bbaccdabd670436ae120a
TabishIzhar <tizhar@iul.ac.in>
2023-08-10 13:23:32 +0530

    Initial commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))       # ../../ two directories up RCFA.py
+sys.path.insert(0, os.path.abspath('../../StrucPy'))       # ../../ two directories up RCFA.py
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
